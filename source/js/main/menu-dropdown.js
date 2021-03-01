5da7c6d1c682a112860176e5b173554743294ae5
katareena <w10160177@gmail.com>
2021-03-01 13:17:54 +0300

    complete js for header, fix style for header

---
+++ source/js/main/menu-dropdown.js
@@ -0,0 +1,30 @@
+'use strict';
+(function () {
+  const ESCAPE = 27;
+  const MAX_TABLET_WIDTH = 1023;
+  const MIN_DESCTOP_WIDTH = 1024;
+  const headerMenu = document.querySelector('.header__menu');
+  const burgerBtn = headerMenu.querySelector('.header__menu-btn--burger-js');
+  const basketBtn = document.querySelector('.header__menu-btn--basket-js');
+  const logo = headerMenu.querySelector('.header__menu-logo');
+  const dropdown = document.querySelector('.header__dropdown');
+
+  function preSetDropdown () {
+    if (document.documentElement.clientWidth <= MAX_TABLET_WIDTH) {
+      dropdown.classList.remove('header__dropdown--open');
+    }
+  }
+
+  function openDropdownHandler () {
+    dropdown.classList.toggle('header__dropdown--open');
+    dropdown.classList.toggle('header__dropdown--absolute');
+    burgerBtn.classList.toggle('header__menu-btn--close');
+    headerMenu.classList.toggle('header__menu--dark-theme');
+    basketBtn.classList.toggle('header__menu-btn--dark-theme');
+    logo.classList.toggle('header__menu-logo--dark-theme');
+  }
+
+  burgerBtn.addEventListener('click', openDropdownHandler)
+  document.addEventListener("DOMContentLoaded", preSetDropdown);
+
+})();
