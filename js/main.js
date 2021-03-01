2d00d7a0ee07644c5104acc323ebc9c36bcb1c6c
katareena <w10160177@gmail.com>
2021-03-01 21:09:32 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 'use strict';
 (function () {
+  const COLOR_ERROR = '#b63616';
+  const input = document.querySelector('#subscription-field');
+  const submitBtn = document.querySelector('.subscription__btn');
+  const message = document.querySelector('#subscription-message');
+
+  function validateInputHandler () {
+    if (input.value == '') {
+      message.style.color = 'transparent';
+      submitBtn.disabled = 'true'
+    } else {
+      if (!input.value.includes('@') || !input.value.includes('.')) {
+        message.style.color = COLOR_ERROR;
+        submitBtn.disabled = 'true'
+      } else {
+        message.style.color = 'transparent';
+        submitBtn.disabled = ''
+      }
+    }
+  }
+
+  input.addEventListener('input', validateInputHandler);
+})();
+
+'use strict';
+(function () {
   const ESCAPE = 27;
   const MAX_TABLET_WIDTH = 1023;
   const MIN_DESCTOP_WIDTH = 1024;
