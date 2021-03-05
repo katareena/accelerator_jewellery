d31e7c21a54e105df9d562d174bd4346102e3a2b
katareena <w10160177@gmail.com>
2021-03-05 17:22:02 +0300

    complete style index.html

--- build/js/main.js
+++ build/js/main.js
@@ -108,84 +108,84 @@
 
 'use strict';
 (function () {
-  var MIN_WIDTH_DESKTOP = 1024;
-  var slides = document.getElementsByClassName('new__slide');
-  var extraDots = document.getElementsByClassName('pagination__item--extra');
-  var slideIndex;
-  var size;
+  // var MIN_WIDTH_DESKTOP = 1024;
+  // var slides = document.getElementsByClassName('new__slide');
+  // var extraDots = document.getElementsByClassName('pagination__item--extra');
+  // var slideIndex;
+  // var size;
 
-  function initSlider() {
-    slideIndex = 1;
+  // function initSlider() {
+  //   slideIndex = 1;
 
-    for (var i = 0; i < slides.length; i++) {
-      slides[i].classList.remove('new__slide--active');
-    }
+  //   for (var i = 0; i < slides.length; i++) {
+  //     slides[i].classList.remove('new__slide--active');
+  //   }
 
 
 
-    if (document.documentElement.clientWidth < MIN_WIDTH_DESKTOP) {
-      size = 2;
-      for (var i = 0; i < size; i++) {
-        slides[i].classList.add('new__slide--active');
-      }
-      for (var i = 0; i < extraDots.length; i++) {
-        extraDots[i].classList.remove('pagination__link--hide');
-      }
+  //   if (document.documentElement.clientWidth < MIN_WIDTH_DESKTOP) {
+  //     size = 2;
+  //     for (var i = 0; i < size; i++) {
+  //       slides[i].classList.add('new__slide--active');
+  //     }
+  //     for (var i = 0; i < extraDots.length; i++) {
+  //       extraDots[i].classList.remove('pagination__link--hide');
+  //     }
 
-    } else {
-      size = 4;
-      for (var i = 0; i < size; i++) {
-        slides[i].classList.add('new__slide--active');
-      }
-      for (var i = 0; i < extraDots.length; i++) {
-        extraDots[i].classList.add('pagination__link--hide');
-      }
-    }
-  }
+  //   } else {
+  //     size = 4;
+  //     for (var i = 0; i < size; i++) {
+  //       slides[i].classList.add('new__slide--active');
+  //     }
+  //     for (var i = 0; i < extraDots.length; i++) {
+  //       extraDots[i].classList.add('pagination__link--hide');
+  //     }
+  //   }
+  // }
 
-  initSlider();
+  // initSlider();
 
-  function moveSlides(n) {
-    var dots = document.getElementsByClassName('pagination__item');
+  // function moveSlides(n) {
+  //   var dots = document.getElementsByClassName('pagination__item');
 
-    if (n > dots.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-      slideIndex = dots.length
-    }
-    for (var i = 0; i < slides.length; i++) {
-      slides[i].classList.remove('new__slide--active');
-    }
-    for (var i = (slideIndex - 1)* size; i < (slideIndex - 1)* size + size; i++) {
-      slides[i].classList.add('new__slide--active');
-    }
+  //   if (n > dots.length) {
+  //     slideIndex = 1
+  //   }
+  //   if (n < 1) {
+  //     slideIndex = dots.length
+  //   }
+  //   for (var i = 0; i < slides.length; i++) {
+  //     slides[i].classList.remove('new__slide--active');
+  //   }
+  //   for (var i = (slideIndex - 1)* size; i < (slideIndex - 1)* size + size; i++) {
+  //     slides[i].classList.add('new__slide--active');
+  //   }
 
-    for (var i = 0; i < dots.length; i++) {
-      dots[i].classList.remove('pagination__item--active');
-    }
-    dots[slideIndex - 1].classList.add('pagination__item--active');
-  }
+  //   for (var i = 0; i < dots.length; i++) {
+  //     dots[i].classList.remove('pagination__item--active');
+  //   }
+  //   dots[slideIndex - 1].classList.add('pagination__item--active');
+  // }
 
-  function prevSlide(evt) {
-    evt.preventDefault();
-    slideIndex = slideIndex - 1;
-    moveSlides(slideIndex);
-  }
+  // function prevSlide(evt) {
+  //   evt.preventDefault();
+  //   slideIndex = slideIndex - 1;
+  //   moveSlides(slideIndex);
+  // }
 
-  function nextSlide(evt) {
-    evt.preventDefault();
-    moveSlides(slideIndex += 1);
-  }
+  // function nextSlide(evt) {
+  //   evt.preventDefault();
+  //   moveSlides(slideIndex += 1);
+  // }
 
-  var prevBtn = document.querySelector('.new__btn--previous');
-  var nextBtn = document.querySelector('.new__btn--next');
+  // var prevBtn = document.querySelector('.new__btn--previous');
+  // var nextBtn = document.querySelector('.new__btn--next');
 
-  prevBtn.addEventListener('click', prevSlide);
-  nextBtn.addEventListener('click', nextSlide);
+  // prevBtn.addEventListener('click', prevSlide);
+  // nextBtn.addEventListener('click', nextSlide);
 
-  document.addEventListener('DOMContentLoaded', initSlider);
-  window.addEventListener('resize', initSlider);
+  // document.addEventListener('DOMContentLoaded', initSlider);
+  // window.addEventListener('resize', initSlider);
 
   // -------- touch slider --------
 
