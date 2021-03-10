4f173bd7d7c107bd080ade3d164d59a4b8b6b996
katareena <w10160177@gmail.com>
2021-03-10 13:32:44 +0300

    add counter.js

--- build/js/main.js
+++ build/js/main.js
@@ -97,6 +97,34 @@
 
 'use strict';
 (function () {
+  const buttons = document.querySelectorAll('.added__btn');
+  const resultBox = document.querySelector('.added__result');
+
+  function changeNumber(evt) {
+    const btnTarget = evt.currentTarget;
+    const result = resultBox.textContent;
+
+    if (btnTarget.classList.contains('added__btn--plus')) {
+      resultBox.textContent = parseInt(result) + 1;
+    }
+
+    if (btnTarget.classList.contains('added__btn--minus')) {
+      if (resultBox.textContent < 1) {
+        resultBox.textContent = parseInt(0);
+      } else {
+        resultBox.textContent = parseInt(result) - 1;
+      }
+    }
+  };
+
+  buttons.forEach(function (el) {
+    el.addEventListener('click', changeNumber);
+  });
+
+})();
+
+'use strict';
+(function () {
   const COLOR_ERROR = '#b63616';
   const input = document.querySelector('#subscription-field');
   const submitBtn = document.querySelector('.subscription__btn');
---
+++ source/js/main/counter.js
@@ -0,0 +1,27 @@
+'use strict';
+(function () {
+  const buttons = document.querySelectorAll('.added__btn');
+  const resultBox = document.querySelector('.added__result');
+
+  function changeNumber(evt) {
+    const btnTarget = evt.currentTarget;
+    const result = resultBox.textContent;
+
+    if (btnTarget.classList.contains('added__btn--plus')) {
+      resultBox.textContent = parseInt(result) + 1;
+    }
+
+    if (btnTarget.classList.contains('added__btn--minus')) {
+      if (resultBox.textContent < 1) {
+        resultBox.textContent = parseInt(0);
+      } else {
+        resultBox.textContent = parseInt(result) - 1;
+      }
+    }
+  };
+
+  buttons.forEach(function (el) {
+    el.addEventListener('click', changeNumber);
+  });
+
+})();
