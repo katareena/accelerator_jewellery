9c13880952038ae85ee1c0409294300bc2fdfced
katareena <w10160177@gmail.com>
2021-03-23 12:40:34 +0300

    remove the class from the content block, rewrite accordion.js

--- build/js/main.js
+++ build/js/main.js
@@ -2,8 +2,6 @@
 
 (function () {
   // --------------- accordion ---------------
-  const items = document.querySelectorAll('.accordion-item');
-
   function moveAccordion (trgt) {
     const set = trgt.dataset.set;
     document.querySelector('#' + set).classList.toggle('hide');
@@ -20,10 +18,28 @@
     }
   };
 
-  items.forEach(el => {
-    el.addEventListener('click', clickPressHandler);
-    el.addEventListener('keydown', enterPressHandler);
-  });
+  function setEventListeners () {
+    let qustions;
+    let filter;
+    if (qustions = document.querySelector('.questions')) {
+      const items = document.querySelectorAll('.accordion-item');
+      items.forEach(el => {
+        el.addEventListener('click', clickPressHandler);
+        el.addEventListener('keydown', enterPressHandler);
+      });
+    }
+
+    if (filter = document.querySelector('.catalog__filter')) {
+      const blocks = document.querySelectorAll('.filter__item');
+      blocks.forEach(el => {
+        const item = el.querySelector('h4');
+        item.addEventListener('click', clickPressHandler);
+        item.addEventListener('keydown', enterPressHandler);
+      });
+    }
+  }
+
+  setEventListeners();
 
 })();
 
--- source/js/main/accordion.js
+++ source/js/main/accordion.js
@@ -1,7 +1,5 @@
 (function () {
   // --------------- accordion ---------------
-  const items = document.querySelectorAll('.accordion-item');
-
   function moveAccordion (trgt) {
     const set = trgt.dataset.set;
     document.querySelector('#' + set).classList.toggle('hide');
@@ -18,9 +16,27 @@
     }
   };
 
-  items.forEach(el => {
-    el.addEventListener('click', clickPressHandler);
-    el.addEventListener('keydown', enterPressHandler);
-  });
+  function setEventListeners () {
+    let qustions;
+    let filter;
+    if (qustions = document.querySelector('.questions')) {
+      const items = document.querySelectorAll('.accordion-item');
+      items.forEach(el => {
+        el.addEventListener('click', clickPressHandler);
+        el.addEventListener('keydown', enterPressHandler);
+      });
+    }
+
+    if (filter = document.querySelector('.catalog__filter')) {
+      const blocks = document.querySelectorAll('.filter__item');
+      blocks.forEach(el => {
+        const item = el.querySelector('h4');
+        item.addEventListener('click', clickPressHandler);
+        item.addEventListener('keydown', enterPressHandler);
+      });
+    }
+  }
+
+  setEventListeners();
 
 })();
